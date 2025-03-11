44151208cadc51b5e8a9d994b1786e87675f3619
fabulous1983 <86053975+fabulous1983@users.noreply.github.com>
2025-03-11 16:53:45 +0100

    Update server.py

--- server.py
+++ server.py
@@ -71,5 +71,5 @@ def generate_graph():
     return jsonify({"image": encoded})
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
