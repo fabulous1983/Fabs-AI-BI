0512c18d11c43af3aad8d81c8a71aed52489b3a9
fabulous1983 <86053975+fabulous1983@users.noreply.github.com>
2025-03-11 16:28:14 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-apt-get update && apt-get install -y unixodbc-dev
+set -e
+
+# Install dependencies
+apt-get update && apt-get install -y --no-install-recommends software-properties-common
+
+# Add a compatible GLIBC version
+add-apt-repository ppa:ubuntu-toolchain-r/test -y
+apt-get update && apt-get install -y --no-install-recommends \
+    gcc-13 \
+    g++-13 \
+    libc6-dev
+
+# Set default GCC version
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 100
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 100
+
+# Verify GLIBC version
+ldd --version
